d71d7ebd53f9016d2148866128e9190e98f0ea64
phamin2001 <amin.pahlavani@gmail.com>
2018-12-13 21:55:04 -0700

    Commit 2. Strings

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
 // Homework - W01d05
 
 // I. Variables & Datatypes
+
+// A. Q + A
+
+// 1. 
+// let variable = value;
+// var variable = value;
+// const variable = value;
+
+// 2.
+// variable = newValue;
+
+// 3.
+// let newVariable = variable;
+
+// 4.
+// declare: telling a program/compiler we need that variable in our program
+// define: clarifying a variabl type, settng a name to it and reserving a memory space in a computer 
+// assign: allocating/reserving a value for a variable
+
+// 5.
+// Pseudocode is a technique that lets developer write their program in simple, 
+// step-by-step instructions using plain English words rather than a programming language.
+
+// We should use it because It allows us to break down a program into simple steps, 
+// like a computer would, without getting errors or distracted by syntax.
+
+// 6.
+// ??
+
+
+
+// B. Strings
+// 1.
+let firstVariable = "Hello World";
+firstVariable = 6;
+let secondVariable = firstVariable;
+secondVariable = "Goodbye! World";
+// firstVariable = 6;
+console.log(firstVariable);
+
+// 2.
+let yourName = "Amin Pahlavani";
+console.log("Hello, my name is " + yourName);
