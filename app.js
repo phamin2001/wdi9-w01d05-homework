42bd2752fe1bc12cfd308cf89375c4c8fe45a5a2
phamin2001 <amin.pahlavani@gmail.com>
2018-12-14 16:01:53 -0700

    Commit. C. Give me Five

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ if(personAge > 16) {
 }
 
 // II. Loops
+
 // A. The basics
 // 1.
 for(let i = 0; i < 11; i++) {
@@ -105,5 +106,15 @@ for(let i = 12; i < 4000; i+=3) {
 for(let i = 0; i < 100; i++) {
     if(i % 2 === 0) {
         console.log(`${i} is an even number.`);
+    }
+}
+
+
+// C. Give me Five
+for(let i = 0; i < 100; i++) {
+    if(i % 5 === 0) {
+        console.log(`I found a ${i}. High five!`);
+    } else if(i % 3 === 0){
+        console.log(`I found a ${i}. Three is a crowd`);
     }
 }
