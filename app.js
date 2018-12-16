5081f42bfa60fd79b136473636b568c61dee6be5
phamin2001 <amin.pahlavani@gmail.com>
2018-12-15 17:15:44 -0700

    Commit I. transmogrify

--- app.js
+++ app.js
@@ -361,3 +361,13 @@ function printLongestWord(arrOfStr) {
 
 console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", 
                             "Peanutbutter", "big", "Todd", "abcdfredtuyh"]));
+
+
+
+
+
+// I. `transmogrify`
+function transmogrify(num1, num2, num3) {
+    return Math.pow((num1 * num2), num3);
+}
+console.log(transmogrify(5, 3, 2));
